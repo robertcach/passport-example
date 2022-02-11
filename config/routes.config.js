a1a261e6806010e32cd5ca082e654108073f229a
Robert Cabrera <cabrera.rch@gmail.com>
2022-02-11 21:06:17 +0100

    User can logout. Lab done

--- config/routes.config.js
+++ config/routes.config.js
@@ -4,16 +4,19 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const usersController = require('../controllers/users.controller');
 
+const authMiddleware = require('../middlewares/auth.middleware')
+
 router.get('/', (req, res, next) => {
   res.render('index')
 })
 
-router.get('/register', authController.register); // Para declarar estas rutas, antes hay que crear el controlador correspondiente
-router.post('/register', authController.doRegister);
-router.post('/login', authController.doLogin);
-router.get('/login', authController.login); // Para declarar estas rutas, antes hay que crear el controlador correspondiente
+router.get('/register', authMiddleware.isNotAuthenticated, authController.register); // Para declarar estas rutas, antes hay que crear el controlador correspondiente
+router.post('/register', authMiddleware.isNotAuthenticated, authController.doRegister);
+router.post('/login', authMiddleware.isNotAuthenticated, authController.doLogin);
+router.get('/login', authMiddleware.isNotAuthenticated, authController.login); // Para declarar estas rutas, antes hay que crear el controlador correspondiente
+router.get('/logout', authMiddleware.isAuthenticated, authController.logout)
 
-router.get('/profile', usersController.profile);
+router.get('/profile', authMiddleware.isAuthenticated, usersController.profile);
 
 
 module.exports = router;
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -56,3 +56,8 @@ module.exports.doLogin = (req, res, next) => {
     }
   })(req, res, next)
 }
+
+module.exports.logout = (req, res, next) => {
+  req.logout();
+  res.redirect('/login');
+}
